a046f612b132e219334d351f773115c81e63f733
Horus Lugo <horusgoul@gmail.com>
2019-02-28 20:01:52 +0100

    Adds Post controller

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,13 +1,78 @@
+import Post from "../entity/post.entity";
+import { Request, Response, NextFunction } from "express";
+import { getRepository } from "typeorm";
+
 class PostController {
-  async create(req, res, next) {}
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const postRepository = getRepository(Post);
 
-  async findAll(req, res, next) {}
+      const postDto: Post = req.body;
+      const post = await postRepository.save(postDto);
 
-  async findOne(req, res, next) {}
+      res.status(200).json(post);
+    } catch (e) {
+      next(e);
+    }
+  }
 
-  async updateOne(req, res, next) {}
+  async findAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const postRepository = getRepository(Post);
 
-  async deleteOne(req, res, next) {}
+      const posts = await postRepository.find();
+
+      res.status(200).json(posts);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async findOne(req: Request, res: Response, next: NextFunction) {
+    try {
+      const postRepository = getRepository(Post);
+
+      const id: number = req.params.id;
+      const post = await postRepository.findOne(id);
+
+      if (!post) {
+        res.status(404).send();
+        return;
+      }
+
+      res.status(200).json(post);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async updateOne(req: Request, res: Response, next: NextFunction) {
+    try {
+      const postRepository = getRepository(Post);
+
+      const id: number = req.params.id;
+      const post: Post = req.body;
+
+      await postRepository.update({ id }, post);
+
+      res.status(200).send();
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async deleteOne(req: Request, res: Response, next: NextFunction) {
+    try {
+      const postRepository = getRepository(Post);
+
+      const id: number = req.params.id;
+      await postRepository.delete({ id });
+
+      res.status(200).send();
+    } catch (e) {
+      next(e);
+    }
+  }
 }
 
 export default new PostController();
